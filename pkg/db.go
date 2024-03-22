e6337f30808d3e0b10f55d84b57ff6cba2030846
aryan <85390033+theredditbandit@users.noreply.github.com>
2024-03-22 22:06:25 +0530

    restructuring

--- pkg/db.go
+++ pkg/db.go
@@ -3,6 +3,9 @@ package pkg
 import (
 	"fmt"
 	"log"
+	"os"
+	"os/user"
+	"path/filepath"
 
 	bolt "go.etcd.io/bbolt"
 )
@@ -28,4 +31,26 @@ func WriteToDB(projLocMap map[string]int, path string) error {
 
 	})
 	return err
+}
+
+// GetDB returns the path to the database file , creating the directory if it doesn't exist
+func GetDB(dbname string) string {
+	usr, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	dbPath := filepath.Join(usr.HomeDir, ".local", "share", "projman", dbname)
+	if _, err := os.Stat(filepath.Dir(dbPath)); os.IsNotExist(err) {
+		os.MkdirAll(filepath.Dir(dbPath), 0755)
+	}
+	return dbPath
+}
+
+
+func GetRecord(key string) int {
+	return 0
+}
+
+func Exists(key string) bool {
+	return false
 }
--- pkg/utils.go
+++ pkg/utils.go
@@ -1,20 +1,12 @@
 package pkg
 
-import (
-	"os"
-	"os/user"
-	"path/filepath"
-)
-
-// GetDB returns the path to the database file , creating the directory if it doesn't exist
-func GetDB(dbname string) string {
-	usr, err := user.Current()
-	if err != nil {
-		panic(err)
-	}
-	dbPath := filepath.Join(usr.HomeDir, ".local", "share", "projman", dbname)
-	if _, err := os.Stat(filepath.Dir(dbPath)); os.IsNotExist(err) {
-		os.MkdirAll(filepath.Dir(dbPath), 0755)
-	}
-	return dbPath
+var StatusMap = map[int]string{
+	0: "Indexed",
+	1: "Started",
+	2: "Paused",
+	3: "Completed",
+	4: "Aborted",
+	5: "Deleted",
+	6: "Currently Working On",
+	7: "Not Started",
 }
