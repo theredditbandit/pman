3db7e43c075b195c8f60ff4d60a36f57ef907b61
aryan <85390033+theredditbandit@users.noreply.github.com>
2024-03-27 21:56:41 +0530

    add info.go

---
+++ cmd/info.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var infoCmd = &cobra.Command{
+	Use:   "info",
+	Short: "The info command prints the README.md file of the specified project.",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("info called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(infoCmd)
+}
