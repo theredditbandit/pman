f01c9512b7fd3235ab323ae470017c3e6dbc047f
aryan <85390033+theredditbandit@users.noreply.github.com>
2024-03-27 16:03:01 +0530

    implement the ls command

--- cmd/ls.go
+++ cmd/ls.go
@@ -2,18 +2,32 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"projman/pkg"
 
 	"github.com/spf13/cobra"
 )
 
 var lsCmd = &cobra.Command{
 	Use:   "ls",
-	Short: "List all projects along with their status",
+	Short: "List all indexed projects along with their status",
+	Long: `List all indexed projects along with their status
+    Usage : projman ls
+    `,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("ls called")
+		data, err := pkg.GetAllRecords(StatusBucket)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for k, v := range data {
+            fmt.Println(v, " : ", k)
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(lsCmd)
+	// TODO: add -f filter flag to only list projects with a certain status
+	// to implement a bucket will be created that maps a status to all the projects with that status
+	// will have to refactor the WriteToDB function to accept an intrface as the data parameter
 }
