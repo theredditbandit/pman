ab86e296565b042399d426827be1c909b5c951c1
aryan <85390033+theredditbandit@users.noreply.github.com>
2024-03-27 14:49:57 +0530

    add status command

---
+++ cmd/status.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"fmt"
+	"projman/pkg"
+
+	"github.com/spf13/cobra"
+)
+
+// statusCmd represents the status command
+var statusCmd = &cobra.Command{
+	Use:   "status",
+	Short: "Get the status of a project",
+	Long:  `Query the database for the status of a project.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) != 1 {
+			fmt.Println("Please provide a project name")
+			return
+		}
+		projName := args[0]
+		status, err := pkg.GetRecord(projName, StatusBucket)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf("Status of %s : %s\n", projName, status)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(statusCmd)
+}
