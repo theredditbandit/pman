8e5ae0eda6885f2a333ca8ded5c61a680a086100
aryan <85390033+theredditbandit@users.noreply.github.com>
2024-05-22 00:10:29 +0530

    Refactor pman interactive UI with minimalist design (#40)
    this removes the need to specify pman ls --c as that is now the default pman ls behaviour

--- cmd/i.go
+++ cmd/i.go
@@ -1,9 +1,14 @@
 package cmd
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/spf13/cobra"
 
+	"github.com/theredditbandit/pman/pkg/db"
 	"github.com/theredditbandit/pman/pkg/ui"
+	"github.com/theredditbandit/pman/pkg/utils"
 )
 
 // iCmd represents the interactive command
@@ -11,11 +16,23 @@ var iCmd = &cobra.Command{
 	Use:     "i",
 	Short:   "Launches pman in interactive mode",
 	Aliases: []string{"interactive", "iteractive"},
-	RunE: func(_ *cobra.Command, _ []string) error {
-		return ui.Tui()
+	RunE: func(cmd *cobra.Command, _ []string) error {
+		filterFlag, _ := cmd.Flags().GetString("f")
+		refreshLastEditTime, _ := cmd.Flags().GetBool("r")
+		data, err := db.GetAllRecords(db.DBName, StatusBucket)
+		if err != nil {
+			return err
+		}
+		if filterFlag != "" {
+			fmt.Println("Filtering by status : ", filterFlag)
+			data = utils.FilterByStatuses(data, strings.Split(filterFlag, ","))
+		}
+		return ui.RenderInteractiveTable(data, refreshLastEditTime)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(iCmd)
+	iCmd.Flags().String("f", "", "Filter projects by status. Usage : pman ls --f <status1[,status2]>")
+	iCmd.Flags().Bool("r", false, "Refresh Last Edited time: pman ls --r")
 }
