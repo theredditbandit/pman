d0d94b1e51b986a2a46ca3cbc922493737a6eb21
aryan <85390033+theredditbandit@users.noreply.github.com>
2024-05-22 14:57:31 +0530

    update version to 1.1.0

--- cmd/root.go
+++ cmd/root.go
@@ -11,7 +11,7 @@ const (
 	ProjectPathBucket  = "projectPaths"
 	ProjectAliasBucket = "projectAliases"
 	ConfigBucket       = "config"
-	version            = "1.0.1"
+	version            = "1.1.0"
 )
 
 var (
